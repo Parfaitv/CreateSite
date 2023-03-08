3c149f05b9d03e53c47058f4e3e6d6e8c1d23deb
parfaitv <parfaitv@student.21-school.ru>
2023-03-09 00:46:54 +0300

    :consrtuction: in development

---
+++ java_script/app.js
@@ -0,0 +1,61 @@
+const cols = document.querySelectorAll('.col')
+
+
+
+document.addEventListener('keydown', (event) => {
+    event.preventDefault()
+    if (event.code.toLowerCase() == 'space') {
+        setRandomColors();
+    }
+})
+
+document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type
+
+    if (type == 'lock') {
+        const node = event.target.tagName.toLowerCase() == 'i' ? event.target : event.target.children[0]
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    } else if (type == 'copy') {
+        copyTextColor(event.target.textContent)
+    }
+})
+
+function generateRandomColor() {
+    const hexCods = '0123456789ABCDEF'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color += hexCods[Math.floor(Math.random() * hexCods.length)]
+    }
+    return '#' + color
+}
+
+function copyTextColor(text) {
+    return navigator.clipboard.writeText(text)
+}
+
+function setRandomColors() {
+    cols.forEach((col) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+        const color = generateRandomColor()
+
+        if (isLocked) {
+            return
+        }
+
+        text.textContent = color
+        col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+setRandomColors()
